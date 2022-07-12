6b521e7a639675e92c1bc91e69ceece8932f15b0
Thomas <thomas-g-smith@hotmail.com>
2022-07-12 08:47:16 -0500

    testing git status

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ import {
   UserPost,
   OthersPost,
 } from "./";
-
+// This is a test
 const App = () => {
   const [loggedIn, setLoggedIn] = useState(false);
   const [singlePost, setSinglePost] = useState("");
